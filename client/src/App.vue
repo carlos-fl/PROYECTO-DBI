8370db4cdf421d84d14e121ffad5fbadfa3d0496
carlos flores <carlosftru@gmail.com>
2024-07-29 10:57:59 -0600

    connection to sql server and some components created

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Input from "./components/Input.vue";
+import Form from "./components/Form.vue";
+import Button from "./components/Button.vue";
+import FormHeader from "./components/FormHeader.vue";
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Form>
+      <FormHeader text="Iniciar sesión"></FormHeader>
+      <Input
+        inputType="text"
+        :maxLength="10"
+        :minLength="3"
+        placeholder="usuario"
+      ></Input>
+      <Input
+        inputType="password"
+        :maxLength="30"
+        :minLength="8"
+        placeholder="contraseña"
+      ></Input>
+      <Button text="Iniciar sesión"></Button>
+    </Form>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ client/src/components/FormHeader.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import { defineProps } from 'vue';
+
+  const props = defineProps({
+    text: String
+  })
+</script>
+
+
+<template>
+  <h4>{{ text }}</h4>
+</template>
+
+
+<style scoped>
+  h4 {
+    text-align: center;
+  }
+</style>
