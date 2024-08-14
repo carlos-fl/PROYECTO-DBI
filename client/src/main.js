420cd1cb9418fa5b54c7a13b90afdf06e8f8e57e
Eros Daniel Rivera <eros.daniel89@gmail.com>
2024-08-14 08:50:52 -0600

    Stash

--- client/src/main.js
+++ client/src/main.js
@@ -3,14 +3,14 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 
-
+/* 
 router.beforeEach((to, from, next) => {
   if (to.meta.requiresAuth) {
     next("/gerente/login");
   } else {
     next();
   }
-});
+}); */
 
 const app = createApp(App)
 app.use(router)
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -36,7 +36,7 @@ const routes = [
     path: '/gerente',
     name: 'GerenteHome',
     component: GerenteHomeView,
-    meta: { requiresAuth: true, }
+    //meta: { requiresAuth: true, }
   }
 ];
 
