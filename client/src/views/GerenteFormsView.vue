a5484a856f59e6710bb641dd72ddfa5a8729e958
Eros Daniel Rivera <eros.daniel89@gmail.com>
2024-08-17 06:03:03 -0600

    Kionda
    Aaaaaaaaa

--- client/src/views/GerenteFormsView.vue
+++ client/src/views/GerenteFormsView.vue
@@ -7,7 +7,7 @@
   import Select from "../components/Select.vue";
   import Option from "../components/Option.vue";
   import Button from "../components/Button.vue";
-  import ProyeccionesForm from "../components/ProyeccionesForm.vue"; //Creo que hare un formulario mas especifico y que llame dinamicamente campos
+  import { BACKEND_URL } from "../config/data";
   
  // components views
  import GerenteEmpleadoView from "./GerenteEmpleadoView.vue";
@@ -20,19 +20,23 @@
 
 </script>
 
+
 <template>
   <div>
-    <!-- Form for managing projections -->
     <div v-if="currentView == 'proyecciones'">
-      <ProyeccionesForm></ProyeccionesForm>
+      <Form>
+        <FormHeader text="Agregar proyección"></FormHeader>
+        <Input></Input>
+        <Input></Input>
+        <Input></Input>
+      </Form>
     </div>
-    <!-- Other forms remain unchanged -->
     <div v-if="currentView == 'precios'">
       <Form>
-        <FormHeader text="Agregar Precio"></FormHeader>
-        <Input placeholder="Nombre del Precio"></Input>
-        <Input placeholder="Valor"></Input>
-        <Button text="Guardar" @handleClick="handleSavePrice"></Button>
+        <FormHeader text="Agregar sala"></FormHeader>
+        <Input></Input>
+        <Input></Input>
+        <Input></Input>
       </Form>
     </div>
     <div v-if="currentView == 'salas'">
@@ -43,34 +47,37 @@
     </div>
     <div v-if="currentView == 'sucursales'">
       <Form>
-        <FormHeader text="Agregar sala"></FormHeader>
-        <Input></Input>
-        <Input></Input>
-        <Input></Input>
+        <FormHeader text="Agregar Sucursal"></FormHeader>
+        <Input placeholder="Nombre de la sucursal" v-model="nombre" />
+        <Input placeholder="Dirección" v-model="direccion" />
+        <Input placeholder="Teléfono" v-model="telefono" />
+        <Button text="Guardar" @click="saveSucursal"></Button>
       </Form>
     </div>
     <div v-if="currentView == 'productos'">
       <Form>
-        <FormHeader text="Agregar sala"></FormHeader>
-        <Input></Input>
-        <Input></Input>
-        <Input></Input>
+        <FormHeader text="Agregar Producto"></FormHeader>
+        <Input placeholder="Nombre del producto" v-model="nombre" />
+        <Input placeholder="Descripción" v-model="descripcion" />
+        <Input placeholder="Precio" v-model="precio" type="number" />
+        <Button text="Guardar" @click="saveProducto"></Button>
       </Form>
     </div>
     <div v-if="currentView == 'combos'">
       <Form>
-        <FormHeader text="Agregar sala"></FormHeader>
-        <Input></Input>
-        <Input></Input>
-        <Input></Input>
+        <FormHeader text="Agregar Combo"></FormHeader>
+        <Input placeholder="Nombre del combo" v-model="nombre" />
+        <Input placeholder="Items incluidos" v-model="items" />
+        <Input placeholder="Precio del combo" v-model="precio" type="number" />
+        <Button text="Guardar" @click="saveCombo"></Button>
       </Form>
     </div>
     <div v-if="currentView == 'clasificaciones'">
       <Form>
-        <FormHeader text="Agregar sala"></FormHeader>
-        <Input></Input>
-        <Input></Input>
-        <Input></Input>
+        <FormHeader text="Agregar Clasificación"></FormHeader>
+        <Input placeholder="Tipo de clasificación" v-model="tipo" />
+        <Input placeholder="Descripción" v-model="descripcion" />
+        <Button text="Guardar" @click="saveClasificacion"></Button>
       </Form>
     </div>
     <div v-if="currentView == 'empleados'">
@@ -91,6 +98,24 @@ function handleSavePrice() {
 function handleSaveRoom() {
   
 }
+
+function saveSucursal() {
+
+  console.log('Saving sucursal:', { nombre: nombre.value, direccion: direccion.value, telefono: telefono.value });
+}
+
+function saveProducto() {
+  console.log('Saving producto:', { nombre: nombre.value, descripcion: descripcion.value, precio: precio.value });
+}
+
+function saveCombo() {
+  console.log('Saving combo:', { nombre: nombre.value, items: items.value, precio: precio.value });
+}
+
+function saveClasificacion() {
+  console.log('Saving clasificacion:', { tipo: tipo.value, descripcion: descripcion.value });
+}
+
 </script>
 
 
